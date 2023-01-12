322fa2b5a9b17c71f0ff32f01ae8429345a5e6ee
azzamsa <me@azzamsa.com>
2023-01-12 22:57:04 +0700

    fix: utoipa can't find `model::<StructName>`

--- src/meta/query.rs
+++ src/meta/query.rs
@@ -4,11 +4,15 @@ use std::sync::Arc;
 use crate::context::ServerContext;
 use crate::meta::model;
 
+// utoipa can't find `model::<StructName>`
+#[allow(unused_imports)]
+use crate::meta::model::MetaResponse;
+
 #[utoipa::path(
     get,
     path = "/meta",
     responses(
-        (status = 200, description = "meta information", body = model::MetaResponse),
+        (status = 200, description = "meta information", body = MetaResponse),
     ),
 )]
 pub async fn meta(
